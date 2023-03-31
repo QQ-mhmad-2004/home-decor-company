ef7ef78a102193c3224ee9e40b3036915a519c2a
Muhammad Qasim <116524995+QQ-mhmad-2004@users.noreply.github.com>
2023-04-01 01:19:34 +0300

    add

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 let navbar = document.getElementById('navbar');
+let button = document.getElementById('buttontop')
 
-window.onscroll = function() {
+window.onscroll = function () {
     if (scrollY >= 150) {
         navbar.style.backgroundColor = '#262626';
         navbar.style.transition = '.8s';
+        button.style.transform = 'none';
     } else {
         navbar.style.backgroundColor = '';
         navbar.style.transition = '.3s';
+        button.style.transform = '';
     }
+}
+
+button.onclick = function () {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 }
