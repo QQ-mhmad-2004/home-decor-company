cc4bc92f25952863c047779aef2bc4476fa1508e
Muhammad Qasim <116524995+QQ-mhmad-2004@users.noreply.github.com>
2023-03-31 06:57:44 +0300

    add

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 let navbar = document.getElementById('navbar');
 
-window.onscroll = function () {
-     if (scrollY >= 150) {
-          navbar.style.backgroundColor = '#262626';
-          navbar.style.transition = '.8s';
-     } else {
-          navbar.style.backgroundColor = '';
-          navbar.style.transition = '.3s';
-     }
+window.onscroll = function() {
+    if (scrollY >= 150) {
+        navbar.style.backgroundColor = '#262626';
+        navbar.style.transition = '.8s';
+    } else {
+        navbar.style.backgroundColor = '';
+        navbar.style.transition = '.3s';
+    }
 }
